f34a253e48a43642e0c826f6ad23634527bbc970
Deepjyoti <deepjyoti0072@gmail.com>
2026-04-09 18:30:33 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Attendance-tracker/", // Change this to your GitHub repo name
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-redux", "@reduxjs/toolkit"],
-          motion: ["framer-motion"],
-        },
-      },
-    },
-  },
+  base: "/Attendance-tracker/",
 });
